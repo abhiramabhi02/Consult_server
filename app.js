4bd815f7db6bf6936908007b98ea4706fd5503a5
Abhi <abhiramabhinv2@gmail.com>
2024-02-20 01:35:52 +0530

    commit 20th feb

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'https://consultifydoc.netlify.app'
-}));
-
+// app.use(cors({
+//   origin: 'https://consultifydoc.netlify.app'
+// }));
+app.use(cors())
 
 
 
